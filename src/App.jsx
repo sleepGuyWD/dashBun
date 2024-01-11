c178851d93b68190bfbfcad41a7f14f169b9e3b7
Warren <warrendira@gmail.com>
2024-01-10 21:43:45 -0800

    Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/assets/vite.svg'
-
-import Nav from './components/nav'
-
+import SidePanel from './components/side-panel'
+import Hero from './components/hero'
+import Footer from './components/footer'
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <Nav />
-      <div>
-        <h1 className="text-center text-3xl font-bold underline">
-        Hello world!
-        </h1>
-        <button className="btn flex justify-center">Button</button>
-      </div>
-    </>
+    <div className="back">
+      <SidePanel />
+      <Hero />
+      <Footer />
+    </div>
   )
 }
 
